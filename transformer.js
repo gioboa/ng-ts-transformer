8f5e195f24a9a31b1f57d6f699e69271cd4c39cd
gioboa <giorgiob.boa@gmail.com>
2019-07-18 12:32:52 +0200

    doc: add README

--- transformer.js
+++ transformer.js
@@ -1,10 +1,18 @@
 'use strict';
 exports.__esModule = true;
 var ts = require('typescript');
+var findDecorator = function(decorators, name) {
+  return (
+    decorators.find(decorator => {
+      let decoratorName = decorator.expression.expression.getText();
+      return decoratorName === name;
+    }) || null
+  );
+};
 var transformer = function(context) {
   return function(rootNode) {
     var visitor = function(node) {
-      if (rootNode.fileName.indexOf('app.component.ts') !== -1 && node.decorators) {
+      if (findDecorator(node.decorators || [], 'Component')) {
         console.log('node', node.decorators.map(d => d.expression.getText()));
       }
       return ts.visitEachChild(node, visitor, context);
