b80165088567a53035d0899f1758cb17caf65ac9
gioboa <giorgiob.boa@gmail.com>
2019-07-18 12:03:49 +0200

    fix: typo

--- transformer.js
+++ transformer.js
@@ -4,7 +4,7 @@ var ts = require('typescript');
 var transformer = function(context) {
   return function(rootNode) {
     var visitor = function(node) {
-      if (rootNode.fileName.indexOf('.component.' !== -1) && node.decorators) {
+      if (rootNode.fileName.indexOf('app.component.ts') !== -1 && node.decorators) {
         console.log('node', node.decorators.map(d => d.expression.getText()));
       }
       return ts.visitEachChild(node, visitor, context);
