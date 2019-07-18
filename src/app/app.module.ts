88371d38517217d1f2101a8fec9dd2c3d4373c6c
gioboa <giorgiob.boa@gmail.com>
2019-07-18 14:21:22 +0200

    dev: create components + modify transformer
    it doesn't work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-
+import { CustomComponent } from './custom/custom.component';
+import { StandardComponent } from './standard/standard.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, CustomComponent, StandardComponent],
+  imports: [BrowserModule],
+  entryComponents: [CustomComponent, StandardComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- transformer.js
+++ transformer.js
@@ -12,12 +12,23 @@ var findDecorator = function(decorators, name) {
 var transformer = function(context) {
   return function(rootNode) {
     var visitor = function(node) {
-      if (findDecorator(node.decorators || [], 'Component')) {
-        console.log('node', node.decorators.map(d => d.expression.getText()));
+      const decorator = findDecorator(node.decorators || [], 'Component');
+      if (!!decorator) {
+        let oldSelector = '';
+        let newSelector = '';
+        if (rootNode.fileName.indexOf(`standard.component.ts`) !== -1) {
+          oldSelector = `selector: 'app-standard'`;
+          newSelector = `selector: 'exclude-app-standard'`;
+        } else if (rootNode.fileName.indexOf(`custom.component.ts`) !== -1) {
+          oldSelector = `selector: 'app-custom'`;
+          newSelector = `selector: 'app-standard'`;
+        }
+        // angular doesn't care about this replace :-(
+        rootNode.text = rootNode.text.replace(oldSelector, newSelector);
+        // console.log(rootNode.text); // debug
       }
       return ts.visitEachChild(node, visitor, context);
     };
-
     return ts.visitNode(rootNode, visitor);
   };
 };
