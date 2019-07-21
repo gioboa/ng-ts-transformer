479f04fda49e33d6ad635d8211fb53e5f29fd8ee
gioboa <giorgiob.boa@gmail.com>
2019-07-21 17:56:52 +0200

    added example

--- transformer.js
+++ transformer.js
@@ -98,3 +98,52 @@ const transformer = context => {
   };
 };
 exports['default'] = transformer;
+
+
+// example
+
+// import * as path from 'path';
+// import * as ts from 'typescript';
+
+// export default function transformer(program: ts.Program): ts.TransformerFactory<ts.SourceFile> {
+//   return (context: ts.TransformationContext) => (file: ts.SourceFile) => visitNodeAndChildren(file, program, context);
+// }
+
+// function visitNodeAndChildren(node: ts.SourceFile, program: ts.Program, context: ts.TransformationContext): ts.SourceFile;
+// function visitNodeAndChildren(node: ts.Node, program: ts.Program, context: ts.TransformationContext): ts.Node;
+// function visitNodeAndChildren(node: ts.Node, program: ts.Program, context: ts.TransformationContext): ts.Node {
+//   return ts.visitEachChild(visitNode(node, program), childNode => visitNodeAndChildren(childNode, program, context), context);
+// }
+
+// function visitNode(node: ts.Node, program: ts.Program): ts.Node {
+//   console.log('2');
+//   const typeChecker = program.getTypeChecker();
+//   if (!isKeysCallExpression(node, typeChecker)) {
+//     return node;
+//   }
+//   if (!node.typeArguments) {
+//     return ts.createArrayLiteral([]);
+//   }
+//   const type = typeChecker.getTypeFromTypeNode(node.typeArguments[0]);
+//   const properties = typeChecker.getPropertiesOfType(type);
+//   return ts.createArrayLiteral(properties.map(property => ts.createLiteral(property.name)));
+// }
+
+// const indexTs = path.join(__dirname, 'index.ts');
+// function isKeysCallExpression(node: ts.Node, typeChecker: ts.TypeChecker): node is ts.CallExpression {
+//   if (!ts.isCallExpression(node)) {
+//     return false;
+//   }
+//   const signature = typeChecker.getResolvedSignature(node);
+//   if (typeof signature === 'undefined') {
+//     return false;
+//   }
+//   const { declaration } = signature;
+//   return (
+//     !!declaration &&
+//     !ts.isJSDocSignature(declaration) &&
+//     path.join(declaration.getSourceFile().fileName) === indexTs &&
+//     !!declaration.name &&
+//     declaration.name.getText() === 'keys'
+//   );
+// }
